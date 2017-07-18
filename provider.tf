c947ebbae85639ad12512969ad4b61fbc29f4041
davetropeano <davetropeano@us.ibm.com>
2017-07-18 10:16:12 -0400

    added sl credentials

--- provider.tf
+++ provider.tf
@@ -1,7 +1,10 @@
 provider "ibm" {
       bluemix_api_key    = "${var.ibm_bx_api_key}"
+      softlayer_username = "${var.sl_username}"
+      softlayer_api_key  = "${var.sl_api_key}"
 }
 
 # Required for the IBM Cloud provider
 variable "ibm_bx_api_key" {}
-
+variable "sl_username" {}
+variable "sl_api_key" {}
